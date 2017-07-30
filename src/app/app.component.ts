1f2ad92f7c2246c0836ad4d428651aafc08940a4
Muhammad Awan <muhammad.adam.awan@gmail.com>
2017-07-31 03:23:59 +1000

    Add properties to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'The Mighty Gods of Olympus';
-  hero = 'Zeus';
+  hero : Hero = {
+    id: 1,
+    name : 'Zeus'
+  };
 }
 
-
+export class Hero {
+  id: number;
+  name: string;
+}
