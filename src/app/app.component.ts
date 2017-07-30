4367ec2931629ad154b27223b31e49b8a53dcf11
Muhammad Awan <muhammad.adam.awan@gmail.com>
2017-07-31 02:58:12 +1000

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'The Mighty Gods of Olympus';
+  hero = 'Zeus';
 }
+
+
