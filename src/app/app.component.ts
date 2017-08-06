14104cc4069dc0e47bf193df2e6fc0413a11d403
Muhammad Awan <muhammad.adam.awan@gmail.com>
2017-08-07 03:06:48 +1000

    Implement hero service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,34 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import { HeroDetailComponent } from './hero-detail.component';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Mighty Gods of Olympus';
+  heroes: Hero[];
   selectedHero: Hero;
-  heroes = HEROES;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroesSlowly(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroesSlowly();
+  }
+
   onSelectaroo(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Hades' },
-  { id: 12, name: 'Kratos' },
-  { id: 13, name: 'Ares' },
-  { id: 14, name: 'Helios' },
-  { id: 15, name: 'Athena' },
-  { id: 16, name: 'Aphrodite' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Hades' },
+  { id: 12, name: 'Kratos' },
+  { id: 13, name: 'Ares' },
+  { id: 14, name: 'Helios' },
+  { id: 15, name: 'Athena' },
+  { id: 16, name: 'Aphrodite' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
